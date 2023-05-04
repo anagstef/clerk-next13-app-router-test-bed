eaf882c2dd0b698123228698d8195d25c1a2cae4
Stefanos Anagnostou <stefanos@clerk.dev>
2023-05-04 21:14:24 +0300

    Use latest clerkjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   console.log(auth());
   return (
-    <ClerkProvider clerkJSUrl={'https://js.lclclerk.com/npm/clerk.browser.js'}>
+    <ClerkProvider>
       <html lang='en'>
         <body className={inter.className}>
           <Links />
